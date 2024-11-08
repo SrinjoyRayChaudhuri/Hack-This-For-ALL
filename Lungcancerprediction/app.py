565750d7fa8412cc88f51e39d555ba3eac6102bf
Srinjoy Ray Chaudhuri <158838641+SrinjoyRayChaudhuri@users.noreply.github.com>
2024-11-08 17:24:09 +0530

    Update app.py

--- Lungcancerprediction/app.py
+++ Lungcancerprediction/app.py
@@ -4,7 +4,7 @@ import joblib
 
 # Load the model and scaler
 model = joblib.load('Lungcancerprediction/best_logistic_model.pkl')
-model = joblib.load('Lungcancerprediction/scaler.pkl')
+scaler = joblib.load("Lungcancerprediction/scaler.pkl")
 
 # Load the data
 data = pd.read_csv('Lungcancerprediction/lung cancer data set.csv.csv')
